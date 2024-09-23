7d8108dc45d3119b9796887697411f83d700c75b
SerJCoL <113114305+SerJCoL@users.noreply.github.com>
2024-09-23 14:40:16 +0300

    Update build-step.sh

--- build-step.sh
+++ build-step.sh
@@ -6,6 +6,6 @@
  docker cp /var/lib/jenkins/workspace/devops-project/. "$container_id":/usr/share/nginx/html
  else
  #docker run -d -p 9889:80 --name web -v ~/site-content:/usr/share/nginx/html nginx
-  docker build -t --name server /var/lib/jenkins/workspace/devops-project
+ docker build -t --name server /var/lib/jenkins/workspace/devops-project
  docker run -d -p 9889:80 server
  fi
