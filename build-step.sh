d12675f2e510337c5d8297ad29e0e21fe2420078
SerJCoL <113114305+SerJCoL@users.noreply.github.com>
2024-09-23 15:12:53 +0300

    Update build-step.sh

--- build-step.sh
+++ build-step.sh
@@ -5,7 +5,6 @@
  if [ -n "$container_id" ]; then
  docker cp /var/lib/jenkins/workspace/devops-project/. "$container_id":/usr/share/nginx/html
  else
- #docker run -d -p 9889:80 --name web -v ~/site-content:/usr/share/nginx/html nginx
- docker build -t server /var/lib/jenkins/workspace/devops-project
- docker run -d -p 9889:80 server
+ docker build -t nginx /var/lib/jenkins/workspace/devops-project
+ docker run -d -p 9889:80 nginx
  fi
